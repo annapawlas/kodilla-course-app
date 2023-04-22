26fb4ad8055f24f3d3074f1aa86fd33ede759253
Anna Pawlas <annapawlas1@gmail.com>
2023-04-22 11:58:33 +0200

    21.4. Zadanie: skrypt do uruchamiania przeglądarki

---
+++ showtasks.bat
@@ -0,0 +1,9 @@
+call runcrud.bat
+
+if "%ERRORLEVEL%" == "0" goto runurl
+echo.
+echo RUNCRUD BUILD has errors – breaking work
+goto fail
+
+:runurl
+start chrome http://localhost:8080/crud/v1/task/tasks
