af5825755bb42ba8dc3cd23f0e980483bf705c51
Anna Pawlas <annapawlas1@gmail.com>
2023-04-23 13:57:54 +0200

    23.3. Zadanie: dodanie maila do wiadomości (ang. CC)

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,47 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class Mail {
+    private final String mailTo;
+    private final String toCc;
+    private final String subject;
+    private final String message;
+
+    public static class MailBuilder{
+        private String mailTo;
+        private String toCc;
+        private String subject;
+        private String message;
+
+        public MailBuilder mailTo(String mailTo){
+            this.mailTo = mailTo;
+            return this;
+        }
+        public MailBuilder toCc(String toCc){
+            this.toCc = toCc;
+            return this;
+        }
+        public MailBuilder subject(String subject){
+            this.subject = subject;
+            return this;
+        }
+        public MailBuilder message(String message){
+            this.message = message;
+            return this;
+        }
+
+        public Mail build(){
+            return new Mail(mailTo, toCc, subject, message);
+        }
+    }
+
+    private Mail(final String mailTo, final String toCc, final String subject, final String message) {
+        this.mailTo = mailTo;
+        this.toCc = toCc;
+        this.subject = subject;
+        this.message = message;
+    }
+}
