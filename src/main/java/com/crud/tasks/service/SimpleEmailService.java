372f7db049e2546439d9196084bdd35ebf57e4ca
Anna Pawlas <annapawlas1@gmail.com>
2023-07-01 14:42:03 +0200

    32.2. Ustawienie pozostałych wartości

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,9 +3,12 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,19 +19,19 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class SimpleEmailService {
 
-//    private final JavaMailSender javaMailSender;
-//
-//    public void send(final Mail mail) {
-//        log.info("Starting email preparation...");
-//        try {
-//            SimpleMailMessage mailMessage = createMailMessage(mail);
-//            javaMailSender.send(mailMessage);
-//            log.info("Email has been sent.");
-//        } catch (MailException e) {
-//            log.error("Failed to process email sending: " + e.getMessage(), e);
-//        }
-//    }
-//
+    private final JavaMailSender javaMailSender;
+
+    public void send(final Mail mail) {
+        log.info("Starting email preparation...");
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+            log.info("Email has been sent.");
+        } catch (MailException e) {
+            log.error("Failed to process email sending: " + e.getMessage(), e);
+        }
+    }
+
 //    private SimpleMailMessage createMailMessage(final Mail mail) {
 //        SimpleMailMessage mailMessage = new SimpleMailMessage();
 //        mailMessage.setTo(mail.getMailTo());
@@ -37,4 +40,26 @@ public class SimpleEmailService {
 //        mailMessage.setText(mail.getMessage());
 //        return mailMessage;
 //    }
+
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        return mailMessage;
+    }
 }
